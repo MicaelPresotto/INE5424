50fb065eec8f31b53aea3d4f651ab3a25de86b49
Eduardo g de lazari <eduardogdelazari2005@gmail.com>
2024-11-03 02:39:11 -0300

    Formata saida do teste 3

--- app/test_p3/test_p3.cc
+++ app/test_p3/test_p3.cc
@@ -6,62 +6,94 @@
 
 using namespace EPOS;
 OStream cout;
+Simple_Spin s;
 
-void function_sqrt() {
-    for (int i = 0; i <= 1e8; i++) {
+#define ITERATIONS  100
+#define N_THREADS   5
+
+void log_stopped(int t) {
+    s.acquire();
+    cout << "|        Thread " << t << " has finished executing.       |" << endl;
+    s.release();
+}
+
+void print_line() {
+    cout << "+-----------------------------------------------+" << endl;
+}
+
+void banner() {
+    print_line();
+    cout << "|  _______        _             _____  _  _     |"    << endl;
+    cout << "| |__   __|      | |           |  __ \\| || |    |"   << endl;
+    cout << "|    | | ___  ___| |_ ___      | |__) | || |_   |"    << endl;
+    cout << "|    | |/ _ \\/ __| __/ _ \\     |  ___/|__   _|  |"  << endl;
+    cout << "|    | |  __/\\__ \\ ||  __/     | |       | |    |"  << endl;
+    cout << "|    |_|\\___||___/\\__\\___|     |_|       |_|    |" << endl;
+    print_line();
+}
+
+int function_sqrt() {
+    for (int i = 0; i <= 1e7; i++) {
         auto sqr = Math::sqrt(i);
         auto gcd = Math::gcd(i, 193);
         sqr += gcd;
     }
+    log_stopped(1);
+    return 0;
 }
 
-void function_delay() {
+int function_delay() {
     Delay d(5e3);
+    log_stopped(2);
+    return 0;
 }
 
 
-void function_pass() {
+int function_pass() {
     for (int i = 0; i < 2e7; i++) {
         i = i;
     }
+    log_stopped(3);
+    return 0;
 }
 
 
-void function_log() {
+int function_log() {
     for (int i = 0; i < 2e7; i++) {
         auto r = Math::fast_log2(i);
         r += 1;
     }
+    log_stopped(4);
+    return 0;
 }
 
-void function_cos() {
+int function_cos() {
     for (int i = 0; i < 2e7; i++) {
         auto r = Math::cos(i);
         r += 1;
     }
+    log_stopped(5);
+    return 0;
 }
 
-
 int main() {
-    RT_Thread* pt1 = new RT_Thread(&function_sqrt, 10e5);
-    RT_Thread* pt2 = new RT_Thread(&function_delay, 7e5);
-    RT_Thread* pt3 = new RT_Thread(&function_pass, 3e5);
-    RT_Thread* pt4 = new RT_Thread(&function_log, 2e5);
-    RT_Thread* pt5 = new RT_Thread(&function_cos, 1e5);
 
-    Delay d(1e7);
+    int i;
+    Periodic_Thread* pthreads[N_THREADS];
+    
+    banner();
 
-    cout << "Deadlines perdidas p1: " << pt1->get_deadline_losses() << endl;
-    cout << "Deadlines perdidas p2: " << pt2->get_deadline_losses() << endl;
-    cout << "Deadlines perdidas p3: " << pt3->get_deadline_losses() << endl;
-    cout << "Deadlines perdidas p4: " << pt4->get_deadline_losses() << endl;
-    cout << "Deadlines perdidas p5: " << pt5->get_deadline_losses() << endl;
+    pthreads[0] = new Periodic_Thread(RTConf(9e5, Periodic_Thread::SAME, Periodic_Thread::UNKNOWN, Periodic_Thread::NOW, ITERATIONS), &function_sqrt);
+    pthreads[1] = new Periodic_Thread(RTConf(9e5, Periodic_Thread::SAME, Periodic_Thread::UNKNOWN, Periodic_Thread::NOW, ITERATIONS), &function_delay);
+    pthreads[2] = new Periodic_Thread(RTConf(9e5, Periodic_Thread::SAME, Periodic_Thread::UNKNOWN, Periodic_Thread::NOW, ITERATIONS), &function_pass);
+    pthreads[3] = new Periodic_Thread(RTConf(9e5, Periodic_Thread::SAME, Periodic_Thread::UNKNOWN, Periodic_Thread::NOW, ITERATIONS), &function_log);
+    pthreads[4] = new Periodic_Thread(RTConf(9e5, Periodic_Thread::SAME, Periodic_Thread::UNKNOWN, Periodic_Thread::NOW, ITERATIONS), &function_cos);
 
-    delete pt1;
-    delete pt2;
-    delete pt3;
-    delete pt4;
-    delete pt5;
+    for (i = 0; i < N_THREADS; i++) pthreads[i]->join();
+    print_line();
+    for (i = 0; i < N_THREADS; i++) cout << "|           Deadlines perdidas p" << i+1 << ": " << pthreads[i]->get_deadline_losses() << "            |" << endl;
+    print_line();
+    for (i = 0; i < N_THREADS; i++) delete pthreads[i];
 
     return 0;
 }
