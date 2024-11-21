5157244d311931c9bafeecf7fc557186b606cb21
MicaelPresotto <mikal2003.sp@gmail.com>
2024-11-21 16:17:26 -0300

    config after reset

--- src/api/thread.cc
+++ src/api/thread.cc
@@ -407,6 +407,9 @@ void Thread::dispatch(Thread * prev, Thread * next, bool charge)
         PMU::reset(4);
         PMU::reset(3);
         PMU::reset(2);
+        PMU::config(4, 24); // L1_INSTRUCTION_CACHE_MISSES
+        PMU::config(3, 15); // Branch Mispredictions
+        PMU::config(2, 2); // Instructions Retired
 
         CPU::switch_context(const_cast<Context **>(&prev->_context), next->_context);
 
